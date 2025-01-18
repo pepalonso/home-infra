798605b6aa16809e27ad1c829e11e023f8748a82
Pep Alonso <pepalonsocosta@gmail.com>
2025-01-19 00:56:24 +0100

    refactor and openai route

--- db/init.sql
+++ db/init.sql
@@ -7,26 +7,27 @@ BEGIN
     START TRANSACTION;
 
     INSERT INTO archived_tasks (
-        task_number, 
-        user_id, 
-        title, 
-        description, 
-        deadline, 
-        importance, 
-        was_completed, 
-        section, 
-        created_at, 
-        archived_at)
-    SELECT 
-        task_number, 
-        user_id, 
-        title, 
-        description, 
-        deadline, 
-        importance, 
-        is_completed AS was_completed, 
-        section, 
-        created_at
+        task_number,
+        user_id,
+        title,
+        description,
+        deadline,
+        importance,
+        was_completed,
+        section,
+        created_at,
+        archived_at
+    )
+    SELECT
+        task_number,
+        user_id,
+        title,
+        description,
+        deadline,
+        importance,
+        is_completed AS was_completed,
+        section,
+        created_at,
         updated_at AS archived_at
     FROM task
     WHERE is_completed = 1 OR is_deleted = 1;
@@ -36,7 +37,9 @@ BEGIN
 
     COMMIT;
 END$$
+
 DELIMITER ;
+
 
 
 CREATE EVENT IF NOT EXISTS ARCHIVE_TASKS

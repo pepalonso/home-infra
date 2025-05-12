7d33d7fea43f7b19cfecd5b7a1242ad687f444cb
Pep Alonso <pepalonsocosta@gmail.com>
2025-05-12 21:53:47 +0200

    enable subdomains

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.10-slim
-
-WORKDIR /app
-COPY . /app
-COPY firebase-key.json /firebase-key.json
-
-RUN pip install --no-cache-dir -r requirements.txt
-CMD ["gunicorn", "-w", "8", "-b", "0.0.0.0:5000", "app:create_app()"]
--- renew_certs.sh
+++ renew_certs.sh
@@ -6,7 +6,7 @@ cd /home/palonso/tasks-server
 docker-compose stop nginx
 
 # Renew the certificates quietly
-sudo certbot renew --quiet
+sudo certbot renew --quiet --cert-name palonso-pi.duckdns.org
 
 # Start the NGINX container again
 docker-compose start nginx
